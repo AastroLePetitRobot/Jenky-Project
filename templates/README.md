6f352fce68a0cc66f99b22000f07ccfc2404271b
hisakiyo <hisakiyo.games@gmail.com>
2020-03-04 23:34:24 +0100

    fix

--- templates/README.md
+++
@@ -1,2 +0,0 @@
-# Super Jenky
-Site du projet tutoré de 2nd année de DUT réalisé grâce à Django et PSQL
--- templates/siteweb/templatetags/nom_item.py
+++
@@ -1,8 +0,0 @@
-from django.template.defaulttags import register
-
-@register.filter
-def get_item(key):
-    key = str(key)
-    with open('siteweb/templatetags/nom.txt') as f:
-        d = dict(x.rstrip().split(None, 1) for x in f)
-    return d.get(key)
